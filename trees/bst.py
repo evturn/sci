7bc7536d37e3fc0ed27215319522530fd9fb193f
Evan Turner <ev@evturn.com>
2018-09-10 22:23:22 -0400

    Create __call__ convenience method for binary tree creation

--- trees/bst.py
+++ trees/bst.py
@@ -1,16 +1,22 @@
 class Node:
-    def __init__(self, info): 
-        self.info = info
+    def __init__(self, data): 
+        self.data = data
         self.left = None
         self.right = None
         self.level = None
 
     def __str__(self):
-        return str(self.info)
+        return str(self.data)
 
 class BinarySearchTree:
     def __init__(self):
         self.root = None
+
+    def __call__(self, vals):
+        for i in range(len(vals)):
+            self.create(vals[i])
+
+        return self
 
     def create(self, val):
         if self.root == None:
@@ -19,13 +25,13 @@ class BinarySearchTree:
             current = self.root
          
             while True:
-                if val < current.info:
+                if val < current.data:
                     if current.left:
                         current = current.left
                     else:
                         current.left = Node(val)
                         break
-                elif val > current.info:
+                elif val > current.data:
                     if current.right:
                         current = current.right
                     else:
